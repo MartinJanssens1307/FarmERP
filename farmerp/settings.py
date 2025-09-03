820a4adee37ccd13f35785754b2aab12d4a84232
MartinJanssens1307 <martin.janssens13@gmail.com>
2025-09-03 12:26:18 +0000

    Allowed Host

--- farmerp/settings.py
+++ farmerp/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = "django-insecure-$1r@13ny-cvfp)q$p)5pzabyq&#^1+19do7lb3no@ddf_+24#y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    farmerp.onrender.com
+]
 
 
 # Application definition
