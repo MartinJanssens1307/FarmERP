060009a0825035c0e43283f3d73cc63d7caf1256
MartinJanssens1307 <martin.janssens13@gmail.com>
2025-09-04 15:11:05 +0000

    Final changes for Debug handling

--- farmerp/settings.py
+++ farmerp/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$1r@13ny-cvfp)q$p)5pzabyq&#^1+19do7lb3no@ddf_+24#y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG') == 'True')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'codespace-url.github.dev']
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 else:
     # Get this from your Render environment variable
     ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
