418abef26fa45cc075784ccc9af57dde486e0395
MartinJanssens1307 <martin.janssens13@gmail.com>
2025-09-08 15:18:14 +0000

    Look and feel improved

--- ERP/urls.py
+++ ERP/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("customers", views.customers, name="customers"),
     path("customers/<int:id>", views.customer, name="customer"),
-    path("crops", views.construction, name="crops")
+    path("crops", views.construction, name="crops"),
+    path("equipment", views.construction, name="equipment"),
+    path("finance", views.construction, name="finance"),
 ]
